333dbd2fed31f78db889ba925f10ea4d00061273
pkoerner6 <112381803+pkoerner6@users.noreply.github.com>
2023-11-19 18:07:50 +0100

    Update project2_code.py

--- project2_code.py
+++ project2_code.py
@@ -1,6 +1,3 @@
-# This serves as a template which will guide you through the implementation of this task.  It is advised
-# to first read the whole template and get a sense of the overall structure of the code before trying to fill in any of the TODO gaps
-# First, we import necessary libraries:
 from tkinter import Grid
 import numpy as np
 import pandas as pd
@@ -58,14 +55,10 @@ def data_loading():
     y_train: array of floats, training output with labels
     X_test: matrix of floats: dim = (100, ?), test input with features
     """
-    # Load training data
+   
     train_df = pd.read_csv("train.csv")
-    
-    # Load test data
     test_df = pd.read_csv("test.csv")
 
-    # TODO: Perform data preprocessing, imputation and extract X_train, y_train and X_test
-    #-------------------------------------------------------------------------
     train_df = replace_nans_with_mean(train_df)
     test_df = replace_nans_with_mean(test_df)
 
@@ -76,7 +69,6 @@ def data_loading():
     y_train = train_df['price_CHF']
     X_train = train_df.drop(columns=['price_CHF'])
     X_test = test_df
-    #-------------------------------------------------------------------------
 
     assert (X_train.shape[1] == X_test.shape[1]) and (X_train.shape[0] == y_train.shape[0]) and (X_test.shape[0] == 100), "Invalid data shape"
 
@@ -97,8 +89,6 @@ def modeling_and_prediction(X_train, y_train, X_test):
     y_test: array of floats: dim = (100,), predictions on test set
     """
 
-    #TODO: Define the model and fit it using training data. Then, use test data to make predictions
-    #-------------------------------------------------------------------------
     parameters = {'kernel':(DotProduct(), RBF(), Matern(), RationalQuadratic(), Matern(), ConstantKernel())} # TODO: Try ExpSineSquared later because alpha crashed
     gpr = GaussianProcessRegressor()
     scorer = make_scorer(r2_score)
@@ -108,17 +98,12 @@ def modeling_and_prediction(X_train, y_train, X_test):
     print(gs_results.best_score_)
     y_pred = gs_results.predict(X_test)
 
-    # #-------------------------------------------------------------------------
     assert y_pred.shape == (100,), "Invalid data shape"
     return y_pred
 
-# Main function. You don't have to change this
 if __name__ == "__main__":
-    # Data loading
     X_train, y_train, X_test = data_loading()
-    # The function retrieving optimal LR parameters
     y_pred=modeling_and_prediction(X_train, y_train, X_test)
-    # Save results in the required format
     dt = pd.DataFrame(y_pred) 
     dt.columns = ['price_CHF']
     dt.to_csv('results.csv', index=False)
